32f27a05cb6d42e10cd0ad87a0bc94d69a548487
Bojan Sala <38323343+bawkee@users.noreply.github.com>
2024-01-18 16:46:29 +0100

    Packaging

---
+++ Source/Desktop/Build-Rel-Packaged.ps1
@@ -0,0 +1,1 @@
+& "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x64 /p:UapAppxPackageBuildMode=StoreUpload /p:AppxBundle=Always /p:AppxPackageDir=Packages\ /p:GenerateAppxPackageOnBuild=true /p:PublishReadyToRun=false /t:Publish

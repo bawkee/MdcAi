83dc424cccb763dc77f3d428cf057dc55e17c7a9
Bojan Sala <bojan.sala@yahoo.com>
2024-01-25 13:08:47 +0100

    Todo update

--- Source/Desktop/MdcAi/Todo.cs
+++ Source/Desktop/MdcAi/Todo.cs
@@ -4,5 +4,7 @@
 // TODO: Vision and DALL-E APIs
 // TODO: Whisper API (text to speech esp.)
 
+// TODO: Include attrib to WinUI and Community Toolkit (these are Microsoft but not really Microsoft)
+
 // BUGS
 // BUG: When an error happens, editing the message wont work anymmore?

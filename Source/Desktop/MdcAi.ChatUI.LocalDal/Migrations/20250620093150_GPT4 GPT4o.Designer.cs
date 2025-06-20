102cec2e5334f2ebaa1047fa7e8f023817756d65
Kim <kim@ktr.co.nz>
2025-06-20 21:44:05 +1200

    Update to net9.0 Update NuGet packages. Install specific packages to update vulnerable packages Add missing db migration Remove PInvoke related code and package

--- Source/Desktop/MdcAi.ChatUI.LocalDal/Migrations/20250620093150_GPT4 GPT4o.Designer.cs
+++ Source/Desktop/MdcAi.ChatUI.LocalDal/Migrations/20250620093150_GPT4 GPT4o.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MdcAi.ChatUI.LocalDal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MdcAi.ChatUI.LocalDal.Migrations
 {
     [DbContext(typeof(UserProfileDbContext))]
-    partial class UserProfileDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250620093150_GPT4 GPT4o")]
+    partial class GPT4GPT4o
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
---
+++ Source/Desktop/MdcAi.ChatUI.LocalDal/Migrations/20250620093150_GPT4 GPT4o.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MdcAi.ChatUI.LocalDal.Migrations
+{
+    /// <inheritdoc />
+    public partial class GPT4GPT4o : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ChatSettings",
+                keyColumn: "IdSettings",
+                keyValue: "general",
+                columns: new[] { "Model", "Premise" },
+                values: new object[] { "gpt-4o", "You are a helpful but cynical and humorous assistant (but not over the top). You give short and straight to the point answers." });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ChatSettings",
+                keyColumn: "IdSettings",
+                keyValue: "general",
+                columns: new[] { "Model", "Premise" },
+                values: new object[] { "gpt-4-1106-preview", "You are a helpful but cynical and humorous assistant (but not over the top). You give short answers, straight, to the point answers. Use md syntax and be sure to specify language for code blocks." });
+        }
+    }
+}
